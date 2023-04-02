858f6c2f7bd060b33f05de53f5fab592066636a4
ahsan <muhammad.ahsan@gmail.com>
2023-04-02 16:04:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [52.52, 13.4050],
+    columns=['lat', 'lon'])
+
+st.map(df)
+
 
 "#### Contact Developer"
 
