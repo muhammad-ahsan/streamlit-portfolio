ae63ad38049d2d79ccfc515eb7e9e7728a8cf265
Muhammad Ahsan <muhammad.ahsan@ginkgo-analytics.com>
2023-04-04 00:39:15 +0200

    minor updates

--- pages/3_Certifications.py
+++ pages/3_Certifications.py
@@ -4,28 +4,12 @@ import streamlit as st
 st.set_page_config(page_title="Certifications")
 
 st.header('Certifications')
-st.subheader('**AWS Certified Solutions Architect – Associate**')
 st.markdown(
     '''
-       - Amazon Web Services
-   '''
-)
-st.subheader('**Professional Scrum Master™ I (PSM I)**')
-st.markdown(
-    '''
-       - scrum.org
-   '''
-)
-st.subheader('**New Models of Business in Society**')
-st.markdown(
-    '''
-       - Coursera
-   '''
-)
-
-st.subheader('**Zertifikat Deutsch / telc Deutsch B1**')
-st.markdown(
-    '''
-       - telc gGmbH
+       - AWS Certified Solutions Architect – Associate
+       - Professional Scrum Master™ I (PSM I)
+       - New Models of Business in Society
+       - Zertifikat Deutsch / telc Deutsch B1
+       
    '''
 )
