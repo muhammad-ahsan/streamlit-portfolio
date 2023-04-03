a44f718845a1939d61b4259edcbafd1854ebc87b
Muhammad Ahsan <muhammad.ahsan@ginkgo-analytics.com>
2023-04-03 23:44:47 +0200

    added courses

--- pages/5_University_Courses.py
+++ pages/5_University_Courses.py
@@ -4,3 +4,41 @@ import streamlit as st
 st.set_page_config(page_title="University Courses")
 
 st.header('University Courses')
+
+st.subheader('Machine Learning')
+st.markdown(
+    '''
+    * Relational Pattern Mining
+    * Artificial Intelligence
+    * Data Warehousing and Data Mining
+    * Advanced Information Retrieval
+    * Advanced Database Systems
+    * Probability and Statistics
+    * Bayesian Networks
+    * Digital Image Processing
+    * Image Data Mining
+    * Intelligent Data Analysis and Complex Systems
+    * Learning Graphical Models
+    * Numerical Machine Learning
+    * Mathematical Optimization
+    * Theory of Automata
+    * Visual Data Mining
+    * Web Intelligence and Big Data
+   '''
+)
+st.subheader('Computer Science and Software Engineering')
+st.markdown(
+    '''
+    * Software Engineering
+    * Design and Analysis of Algorithms
+    * Computer Architecture
+    * Computer Communications
+    * Computer Graphics
+    * Data Structures
+    * Database Systems
+    * Advanced Recommender Systems
+    * Semantic Web
+    * Logic and Knowledge Representation
+
+   '''
+)
