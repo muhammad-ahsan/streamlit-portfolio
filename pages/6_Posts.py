7e1f1358cd0c4f451606827f67495415b9e78090
Muhammad Ahsan <muhammad.ahsan@ginkgo-analytics.com>
2023-04-05 03:56:54 +0200

    Updated posts

--- pages/6_Posts.py
+++ pages/6_Posts.py
@@ -4,3 +4,12 @@ import streamlit as st
 st.set_page_config(page_title="Posts")
 
 st.title('Posts')
+
+
+st.markdown(
+    """
+        - [Data Science and Software Development Cohesion](https://medium.com/@muhammad.ahsan/data-science-and-software-development-cohesion-e06faba3439a)
+        - [Machine Learning + Unlearning with Data Distribution Drift](https://medium.com/@muhammad.ahsan/machine-learning-unlearning-with-data-distribution-drift-3cdeb93d1380)
+        - [Erasmus Mundus: A life long Experience](https://medium.com/@muhammad.ahsan/erasmus-mundus-a-life-long-experience-40e628d3223a)
+    """
+)
