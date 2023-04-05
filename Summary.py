46db89c0825042378daa6de2bb2518ea449fb327
Muhammad Ahsan <muhammad.ahsan@ginkgo-analytics.com>
2023-04-05 03:23:22 +0200

    minor update

--- Summary.py
+++ Summary.py
@@ -3,17 +3,28 @@ import streamlit as st
 st.title('Muhammad Ahsan (He/Him)')
 st.markdown('**Lead Machine Learning Engineer | Data Solutions Architect | Data Evangelist**')
 
-st.subheader('Summary')
+st.subheader('About Me')
+st.markdown('I''m a proud "multikulti" human being located in Berlin (52.524° N, 13.410° E)')
 
+st.markdown('''
+            
+    - 💬 Favourite Quote "You were born with wings, why prefer to crawl through life?" by Rumi
+    - 🔭 I’m currently working on multiple data science and machine learning initiatives
+    - 👨 I solve business problem via data and machine learning powered software solutions
+    - ❤️ I enjoy designing cloud solutions for ML driven applications
+    - ⏱ In my free time, I read about technological advancements in science and engineering
+    - 🌱 I’m specialized in advanced MLOps and experimenting with deep reinforcement learning
+    - 📫 How to reach me: muhammad.ahsan@gmail.com
+    
+''')
+
+st.subheader('Professional Experience')
 st.markdown(
-    """
+    '''
         - Experienced data professional with 12 years of demonstrable professional work history across Europe and Pakistan.
         - Skilled in Machine Learning, MLOps, Data Architecture, Data Science and Engineering.
         - Strong expertise in transforming business challenges into tangible data solutions.
         - Computer Science background with experience of backend development.
         - Erasmus Mundus Master's degree in Data Mining and Knowledge Management from France and Italy
-    """
+    '''
 )
-
-
-
