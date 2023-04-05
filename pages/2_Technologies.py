da9016cc359e552d91833dedb7a770a8ce0c25da
Muhammad Ahsan <muhammad.ahsan@ginkgo-analytics.com>
2023-04-05 11:29:38 +0200

    minor update

--- pages/2_Technologies.py
+++ pages/2_Technologies.py
@@ -1,3 +1,55 @@
 import streamlit as st
 
 st.title('Technologies')
+
+st.subheader('**Programming Languages**')
+st.markdown(
+    '''
+       - Python
+       - SQL
+       - Java
+       - C/C++ 
+   '''
+)
+st.subheader('**Machine Learning APIs**')
+st.markdown(
+    '''
+       - Tensorflow
+       - scikit-learn
+   '''
+)
+st.subheader('**Cloud Technologies**')
+
+st.markdown(
+    '''
+        - Amazon Web Services (AWS) 
+        - AWS Glue 
+        - Amazon Athena 
+        - AWS Lambda
+        - Amazon Kinesis 
+        - Amazon Elastic MapReduce (EMR) 
+        - AWS SageMaker 
+        - Amazon ECS
+        - GCP Cloud Run 
+        - Amazon Relational Database Service (RDS) 
+   '''
+)
+st.subheader('**Big Data Platform**')
+st.markdown(
+    '''
+        - Apache Spark
+        - Spark SQL
+        - Hadoop
+        - HDFS
+        - Presto / Hive 
+   '''
+)
+st.subheader('**Other**')
+st.markdown(
+    '''
+        - Jenkins 
+        - Docker 
+        - Kubernetes 
+   '''
+)
+
